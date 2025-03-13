20b9124fda45503814115976e89575f2fcc2ae29
AliSherZ <zabiullah.alisher@gmail.com>
2025-03-13 11:02:04 +0300

    created procedure for inserting data

--- sql-scripts/create_tables.sql
+++ sql-scripts/create_tables.sql
@@ -1,37 +1,59 @@
+-- Active: 1741849904817@@127.0.0.1@5432@csm
+create table suppliers(
+    id int generated always as identity primary key,
+    name varchar(100) not null,
+    phone varchar(15),
+    email varchar(50),
+    address text
+);
+
 create table customers(
     id int generated always as identity primary key,
-    name varchar(255) not null,
-    phone varchar(50),
+    name varchar(100) not null,
+    phone varchar(15),
     email varchar(50),
-    address varchar(255)
+    address text
 );
 
 create table products(
     id int generated always as identity primary key,
-    name varchar(255) not null,
+    name varchar(250) not null,
     description text
-);
-
-create table prices(
-    id int generated always as identity primary key,
-    prod_id int references products(id) on delete cascade,
-    purchase float default 0,
-    sale float default 0
-);
-
-create table suppliers(
-    id int generated always as identity primary key,
-    name varchar(255) not null,
-    phone varchar(50),
-    email varchar(50),
-    address varchar(255)
 );
 
 create table inventories(
     id int generated always as identity primary key,
-    prod_id int references products(id) on delete cascade not null,
+    date date default current_date,
     description text,
     quantity int not null default 0,
-    date date not null default current_date,
-    sup_id int references suppliers(id) on delete cascade not null
+    prod_id int references products(id) on delete cascade,
+    sup_id int references suppliers(id) on delete cascade
 );
+
+create table prices(
+    inv_id int not null references inventories(id) on delete cascade,
+    prod_id int not null references products(id) on delete cascade,
+    purchase float not null default 0,
+    sale float not null default 0,
+    current boolean not null default false,
+    primary key (inv_id, prod_id)
+);
+
+create table sales(
+    id int generated always as identity primary key,
+    date date not null default current_date,
+    description text,
+    quantity int not null default 0,
+    other_price float not null default 0,
+    inv_id int not null,
+    prod_id int not null,
+    cust_id int not null references customers(id) on delete cascade,
+    foreign key (inv_id, prod_id) references prices(inv_id, prod_id)
+);
+
+create table receivables(
+    id int generated always as identity primary key,
+    received float not null default 0,
+    date date not null default current_date,
+    cust_id int references customers(id) on delete cascade
+)
---
+++ sql-scripts/insert_data.sql
@@ -0,0 +1,77 @@
+-- Active: 1741849904817@@127.0.0.1@5432@csm
+
+create procedure pr_insert_supplier(_name varchar, _phone varchar, _email varchar, _address varchar) language plpgsql as $$
+    begin
+        if trim(_phone) = '' then _phone := null; end if;
+        if trim(_email) = '' then _email := null; end if;
+        if trim(_address) = '' then _address := null; end if;
+
+        insert into suppliers(name, phone, email, address)
+        values(_name, _phone, _email, _address);
+    end;
+$$;
+
+create procedure pr_insert_customer(_name varchar, _phone varchar, _email varchar, _address varchar) language plpgsql as $$
+    begin
+        if trim(_phone) = '' then _phone := null; end if;
+        if trim(_email) = '' then _email := null; end if;
+        if trim(_address) = '' then _address := null; end if;
+
+        insert into customers(name, phone, email, address)
+        values(_name, _phone, _email, _address);
+    end;
+$$;
+
+create procedure pr_insert_product(_name varchar, _description text) language plpgsql as $$
+    begin
+        if trim(_description) = '' then _description := null; end if;
+
+        insert into products(name, description)
+        values(_name, _description);
+    end;
+$$;
+
+create procedure pr_insert_price(_inv_id int, _prod_id int, _purchase float, _sale float, _current boolean) language plpgsql as $$
+    begin
+        if _current = true then
+            update prices
+            set current = false
+            where inv_id = _inv_id and prod_id = _prod_id;
+        end if;
+
+        insert into prices(inv_id, prod_id, purchase, sale, current)
+        values(_inv_id, _prod_id, _purchase, _sale, _current);
+    end;
+$$;
+
+create procedure pr_insert_inventory( _date date, _description text, _quantity int, _prod_id int, _sup_id int, _purchase float, _sale float, _current boolean) language plpgsql as $$
+    declare
+        inventory_id int;
+    begin
+        if trim(_description) = '' then _description := null; end if;
+
+        insert into inventories("date", "description", quantity, prod_id, sup_id)
+        values (_date, _description, _quantity, _prod_id, _sup_id)
+        returning id into inventory_id;
+
+        call pr_insert_price(inventory_id, _prod_id, _purchase, _sale, _current);
+    end;
+$$;
+
+create procedure pr_insert_receivable(_received float, _date date, _cust_id int) language plpgsql as $$
+    begin
+        insert into receivables(received, "date", cust_id)
+        values(_received, _date, _cust_id);
+    end;
+$$;
+
+create procedure pr_insert_sale(_date date, _description text, _quantity int, _other_price float, _inv_id int, _prod_id int, _cust_id int, _received float) language plpgsql as $$
+    begin
+        if trim(_description) = '' then _description := null; end if;
+
+        insert into sales("date", "description", quantity, other_price, inv_id, prod_id, cust_id)
+        values(_date, _descripton, _quantity, _other_price, _inv_id, _prod_id, _cust_id);
+
+        call pr_insert_receivable(_received, _date, _cust_id);
+    end;
+$$;
