7a553f5a256d79e53a461a92f387b59c73942459
AliSherZ <zabiullah.alisher@gmail.com>
2025-03-13 17:19:00 +0300

    fn_get_products is added

--- sql-scripts/select_data.sql
+++ sql-scripts/select_data.sql
@@ -1,20 +1,4 @@
--- customer
-
-select id, name, phone, address from customers;
-
-select p.id, p.name, sum(i.quantity) as inventory, sum(s.quantity) as sold
-from
-    products as p
-    join inventories as i on i.prod_id = p.id
-    join sales as s on s.prod_id = p.id
-group by
-    p.id,
-    p.name;
-
-select * from inventories;
-
-select * from sales;
-
+create view vw_products as
 with
     inventory as (
         select prod_id, sum(quantity) as stock
@@ -31,20 +15,59 @@ with
 select
     p.id,
     p.name,
-    coalesce(i.stock, 0),
-    coalesce(s.sold, 0),
+    p.description,
+    coalesce(i.stock, 0) as stocked,
+    coalesce(s.sold, 0) as sold,
     coalesce(i.stock, 0) - coalesce(s.sold, 0) as in_stock,
+        (
+        select sale
+        from prices
+        where
+            prod_id = p.id
+            and "current" = true
+    ) as unit_price,
     (
         select sale
         from prices
         where
             prod_id = p.id
             and "current" = true
-    ) * (coalesce(i.stock, 0) - coalesce(s.sold, 0)) as "value"
+    ) * (
+        coalesce(i.stock, 0) - coalesce(s.sold, 0)
+    ) as total_price
 from
     products as p
     left join inventory as i on i.prod_id = p.id
     left join sale as s on s.prod_id = p.id
-    order by p.id;
+order by p.id;
 
-select * from prices;
+-- select * from vw_products;
+
+create function fn_get_products () returns table (
+    id int,
+    name varchar,
+    description text,
+    quantity jsonb,
+    price jsonb
+) language plpgsql
+as $$
+    begin
+        return query
+        select 
+            vw_products.id,
+            vw_products.name,
+            vw_products.description,
+            jsonb_build_object(
+                'in',stocked,
+                'out', sold,
+                'left',in_stock
+            ) as quantity,
+            jsonb_build_object(
+                'unit',unit_price,
+                'total',total_price
+            )
+        from vw_products;
+    end;
+$$;
+
+select * from fn_get_products();
