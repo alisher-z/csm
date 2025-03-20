36b70e55a2219bef7530e3aead1e925f0218875d
AliSherZ <zabiullah.alisher@gmail.com>
2025-03-20 16:51:33 +0300

    working on sales receipt

--- sql-scripts/sales_receipt.sql
+++ sql-scripts/sales_receipt.sql
@@ -1,6 +1,23 @@
-create table sales_receipt(
+create table sales_receipts(
     id int generated always as identity primary key,
     "name" varchar(50),
+    cust_id int not null references customers(id) on delete cascade
+);
+
+create table receivables(
+    id int generated always as identity primary key,
     "date" date not null,
-    customer int not null references customers(id) on delete cascade
-)
+    received float not null default 0,
+    "description" text,
+    recp_id int references sales_receipts(id) on delete cascade
+);
+
+create table sales(
+    id int generated always as identity primary key,
+    "description" text,
+    quantity int not null default 0,
+    other_price float not null default 0,
+    inv_id int not null,
+    prod_id int not null,
+    foreign key (inv_id, prod_id) references prices(inv_id, prod_id)
+);
