1bef8fd569b1c6017970c86ea9153ab134bd9d82
AliSherZ <zabiullah.alisher@gmail.com>
2025-03-25 23:35:32 +0300

    committed

--- frontend/src/app/components/dropdown/list/list.component.ts
+++ frontend/src/app/components/dropdown/list/list.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, inject, TemplateRef } from '@angular/core';
+import { DropdownService } from '../dropdown.service';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'dropdown-list',
-  imports: [],
+  imports: [NgTemplateOutlet],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class DropdownListComponent {
+  @ContentChild('line') line: TemplateRef<any> | null = null;
 
+  service = inject(DropdownService);
+
+  get filtered() {
+    return this.service.filtered();
+  }
 }
